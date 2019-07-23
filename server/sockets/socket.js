e8c08f57376837d2e1340dc35bfbae2c668d859d
Nicolas Sepulveda Alvear <ni.sepulvedaa@gmail.com>
2019-07-23 17:31:37 -0400

    Primer Commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,58 @@
+const { io } = require('../server');
+
+const { Usuarios } = require('../clases/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    //console.log('Usuario conectado');
+    client.on('entrarChat', (data, callback) => {
+
+        // console.log(data);
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre es necesario'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        return callback(usuarios.getPersonasPorSala(data.sala));
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    });
+
+    //console.log('Usuario conectado');
+    client.on('disconnect', () => {
+
+        let PersonaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(PersonaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${PersonaBorrada.nombre} salio`));
+        client.broadcast.to(PersonaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(PersonaBorrada.sala));
+    });
+
+    //mensajes privados
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+    });
+
+
+});
