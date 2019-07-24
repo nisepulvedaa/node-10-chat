e611a3c5c987985cc4f79d630ff95eecca132498
Nicolas Sepulveda Alvear <ni.sepulvedaa@gmail.com>
2019-07-24 01:03:17 -0400

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,25 +1,28 @@
 var socket = io();
-var params = new URLSearchParams(window.location.search);
 
+var sala = params.get('sala');
+
+var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-
     nombre: params.get('nombre'),
     sala: params.get('sala')
-
 };
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
 
-        console.log('Usuarios conectados', resp);
-
+        renderizarUsuarios(resp);
+        // console.log('Usuarios conectados', resp);
     });
 
 });
@@ -34,32 +37,27 @@ socket.on('disconnect', function() {
 
 // Enviar información
 /* socket.emit('crearMensaje', {
-    usuario: 'Nicolas',
-    mensaje: 'Hola Mundo'
+    nombre: nombre,
+    mensaje: txtMensaje.val()
 }, function(resp) {
     console.log('respuesta server: ', resp);
 }); */
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
 });
 
-
-//Escuchar cambios de usuarios
-//cuando un usuaraio entra o sale del chaat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-
-//Mensajes Privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje Privado', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 
 });
